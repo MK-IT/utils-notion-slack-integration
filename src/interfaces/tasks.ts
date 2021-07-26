0c63e8ac948b8676990734ae9d869dfae8afb2d8
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2021-07-26 11:52:52 +0300

    feat: implement tasks interface

---
+++ src/interfaces/tasks.ts
@@ -0,0 +1,22 @@
+import { User } from "@notionhq/client/build/src/api-types";
+
+export interface Task {
+  name: string;
+  dueDate?: Date;
+  url: string;
+}
+
+export interface CreateTaskParams {
+  title: string;
+  type: string; //type id
+  sprint: string; // sprint id
+  status: string; //status id
+  priority: string; // priority id
+  estimate: string; // story points Id
+  accountable: User;
+  reviewer: User;
+  timeline: {
+    start: string;
+    end?: string;
+  };
+}
