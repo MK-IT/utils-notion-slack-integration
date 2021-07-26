feb43247afa7bb1fd37bdd334748f07b4cba57db
Martin Angelov <martoy123691@gmail.com>
2021-07-27 01:47:45 +0300

    #5 refactor: mapNotionPageToTask util function

--- src/services/notion/utils/index.ts
+++ src/services/notion/utils/index.ts
@@ -1,51 +1,38 @@
-import { PropertyValueMap } from "@notionhq/client/build/src/api-endpoints";
+import { DropdownValues, InnerValue } from '../../../interfaces/notionValues';
+import { Task } from '../../../interfaces/tasks';
 
-import { DropdownValues, InnerValue } from "../../../interfaces/notionValues";
-import { Task } from "../../../interfaces/tasks";
-
-export const mapNotionPageToTask = ({
-  url,
-  properties,
-}: {
-  url: string;
-  properties: PropertyValueMap;
-}): Task => {
-  // FIXME: any type
-  const props = properties as any;
+// TODO: fix any type
+export const mapNotionPageToTask = (
+  page: any): Task => {
   const task: Task = {
-    name: props.projects.title[0]?.text?.content,
-    url,
-    dueDate: props.timeline?.date?.end,
+    name: page.properties.Projects.title[0]?.text?.content,
+    url: page.url,
+    dueDate: new Date(page.properties.Timeline.date.end)
   };
   return task;
 };
 
 // FIXME: SHAME
-export const mapNotionPropertiesToDropdownValues = (
-  properties: any
-): DropdownValues => {
-  const columnNames = ["Status", "Type", "Priority", "Estimate", "Sprint"];
-  const notionValues: any = columnNames.map((column) => {
+export const mapNotionPropertiesToDropdownValues = (properties: any): DropdownValues => {
+  const columnNames = ['Status', 'Type', 'Priority', 'Estimate', 'Sprint'];
+  const notionValues: any = columnNames.map(column => {
     const dropdownValues: InnerValue[] = (
-      properties[column].select
-        ? properties[column].select
-        : properties[column].multi_select
+      properties[column].select ? properties[column].select : properties[column].multi_select
     ).options.map(({ id, name }: { id: string; name: string }) => ({
       id,
-      text: name,
+      text: name
     }));
     // story points have whitespace
-    const dropdownName = column.split(" ").join("").toLowerCase();
+    const dropdownName = column.split(' ').join('').toLowerCase();
 
     const defaultValue = dropdownValues.find(
-      (p) =>
-        p.id === process.env[`NOTION_DEFAULT_${dropdownName.toUpperCase()}_ID`]
+      p => p.id === process.env[`NOTION_DEFAULT_${dropdownName.toUpperCase()}_ID`]
     );
     return {
       [dropdownName]: {
         values: dropdownValues,
-        defaultValue,
-      },
+        defaultValue
+      }
     };
   });
   // TODO: Set initialValue for sprints;
