bc079a5cb337a60e74aea890bb74d9d4555906f4
Martin Angelov <martin.angelov@mkit.io>
2021-07-29 16:13:36 +0300

    fix(#7): eslintrc.js rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,8 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json'
   },
-  overrides: {
-    rules: {
-      'import/prefer-default-export': 'off'
-    }
+  rules: {
+    'import/prefer-default-export': 'off'
   },
   plugins: ['@typescript-eslint'],
   extends: ['airbnb-typescript/base', 'prettier']
