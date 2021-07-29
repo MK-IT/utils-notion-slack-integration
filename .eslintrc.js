92ae0f54456c6d054aef51ff9a7e7c9737212134
Martin Angelov <martin.angelov@mkit.io>
2021-07-29 16:38:31 +0300

    refactor(#7): eslintrc overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,21 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json'
+    project: ['./tsconfig.json']
   },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     'import/prefer-default-export': 'off'
   },
+  overrides: [
+    {
+      files: ['*.js, *.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json']
+      }
+    }
+  ],
   plugins: ['@typescript-eslint'],
   extends: ['airbnb-typescript/base', 'prettier']
 };
