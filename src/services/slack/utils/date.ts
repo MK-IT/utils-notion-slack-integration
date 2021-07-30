7e84bd3a2e8844c2cfa3bcf90966702fe4a40919
=amartinn <martoy123691@gmail.com>
2021-07-30 23:24:56 +0300

    feat(#9): date parsing util

--- src/services/slack/utils/date.ts
+++ src/services/slack/utils/date.ts
@@ -3,3 +3,9 @@ export const formatDate = (date : Date) : string => {
   const formattedDate = new Date().toISOString().slice(0, 10);
   return formattedDate;
 };
+
+// parse formatted date (yyy-mm-dd) to date object
+export const parseDate = (date: string) : Date => {
+  const parsedDate =  new Date(date);
+  return parsedDate;
+ }
