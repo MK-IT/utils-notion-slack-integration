a131bc70a06c4d68f16758c38813cc8173bfc479
=amartinn <martoy123691@gmail.com>
2021-07-31 16:03:26 +0300

    chore(#8): wire up list tasks command blocks

--- src/services/slack/utils/commands/listTasks.ts
+++ src/services/slack/utils/commands/listTasks.ts
@@ -1,4 +1,23 @@
+import { AckFn, App, RespondArguments, SlashCommand } from '@slack/bolt';
 
-export const triggerTaskListingCommand = () => {
-  console.log('trigger task listing cmd...');
+import * as Notion from '@services/notion';
+import { getListTasksBlocks } from '@services/slack/views/listTasks';
+export const triggerTaskListingCommand = async (
+  client: App,
+  ack: AckFn<string | RespondArguments>,
+  payload: SlashCommand
+) => {
+  ack();
+  const { user_id, channel_id } = payload;
+  const {
+    profile: { email }
+  } = await client.client.users.profile.get({ user: user_id });
+  const notionUser = await Notion.getUserByEmail(email);
+  const incompleteTasks = await Notion.getIncompleteTasksByUserId(notionUser);
+  const incompleteTasksBlocks = getListTasksBlocks(incompleteTasks);
+  await client.client.chat.postEphemeral({
+    channel: channel_id,
+    user: user_id,
+    blocks: incompleteTasksBlocks
+  });
 };
