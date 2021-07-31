9aa5844991e910b341b0563c460f5c7f8da94179
=amartinn <martoy123691@gmail.com>
2021-07-31 15:04:42 +0300

    feat(#8): implement create task command

--- src/services/slack/utils/commands/createTask.ts
+++ src/services/slack/utils/commands/createTask.ts
@@ -1,3 +1,13 @@
-export const triggerCreateTaskCommand = () => {
-  console.log('trigger creating task cmd...');
+import { AckFn, App, RespondArguments, SlashCommand } from "@slack/bolt";
+
+import * as Notion from '@services/notion';
+import {tasks} from '@services/slack/views/tasks'
+export const triggerCreateTaskCommand = async (client: App, ack: AckFn<string | RespondArguments> , payload: SlashCommand) => {
+  ack();
+  const dropdownValues = await Notion.getSlackCreateTaskViewValues();
+  // opens add task modal
+  await client.client.views.open({
+    trigger_id: payload.trigger_id,
+    view: tasks.createTask(dropdownValues, payload.user_id)
+  });
 };
