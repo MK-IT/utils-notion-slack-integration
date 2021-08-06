d245fde0d6e05320eede3b1d8234b36e1c53681b
Martin Angelov <martoy123691@gmail.com>
2021-08-06 11:33:24 +0300

    feat(view-actions): create mapping function from submit values to create task params

--- src/services/slack/actions/createTaskAction.ts
+++ src/services/slack/actions/createTaskAction.ts
@@ -1,10 +1,51 @@
-import { App, ViewSubmitAction } from '@slack/bolt';
+import { App, ViewStateValue, ViewSubmitAction } from '@slack/bolt';
+
+import * as Notion from '@services/notion';
+import { CreateTaskParams } from '@interfaces/tasks';
+import { User } from '@notionhq/client/build/src/api-types';
+
+const mapCreateTaskSubmitValuesToParams = (
+  values: {
+    [blockId: string]: {
+      [actionId: string]: ViewStateValue;
+    };
+  },
+  accountable: User,
+  reviewer: User
+): CreateTaskParams => ({
+  accountable,
+  reviewer,
+  title: values.title.title.value,
+  description: values.description.description.value,
+  type: values.type.type.selected_option.value,
+  status: values.status.status.selected_option.value,
+  priority: values.priority.priority.selected_option.value,
+  estimate: values.estimate.estimate.selected_option.value,
+  sprint: values.sprint.sprint.selected_option.value,
+  timeline: {
+    start: values.start.start.selected_date,
+    end: values.end.end.selected_date
+  }
+});
 
 export const createTaskAction = (slackClient: App) => {
   slackClient.view<ViewSubmitAction>(
     { callback_id: 'submit-form' },
-    async ({ ack /* , payload */ }) => {
+    async ({ ack, payload, client }) => {
       ack();
+      const { values } = payload.state;
+      const slackAccountableUserEmail = await (
+        await client.users.profile.get({ user: values.accountable.accountable.selected_user })
+      ).profile.email;
+      const accountable = await Notion.getUserByEmail(slackAccountableUserEmail);
+
+      const slackReviewerUserEmail = await (
+        await client.users.profile.get({ user: values.reviewer.reviewer.selected_user })
+      ).profile.email;
+      const reviewer = await Notion.getUserByEmail(slackReviewerUserEmail);
+      const params = mapCreateTaskSubmitValuesToParams(values, accountable, reviewer);
+
+      await Notion.createTask(params);
       // TODO: check if timeline start date is less than today and send error msg
       // TODO: check if timeline end date is less than start date and send error msg
       // Example error handling:
