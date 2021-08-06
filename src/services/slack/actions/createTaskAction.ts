d5e751d508db9c53e4583bae8329481e3ed8d640
Martin Angelov <martoy123691@gmail.com>
2021-08-06 16:58:05 +0300

    feat(form-actions): implement create task action

--- src/services/slack/actions/createTaskAction.ts
+++ src/services/slack/actions/createTaskAction.ts
@@ -3,6 +3,8 @@ import { User } from '@notionhq/client/build/src/api-types';
 
 import * as Notion from '@services/notion';
 import { CreateTaskParams } from '@interfaces/tasks';
+import { createTaskSchema } from '../validations';
+import { dividerBlock, sectionBlock, sectionBlockUrl } from '../blocks';
 
 const mapCreateTaskSubmitValuesToParams = (
   values: {
@@ -32,34 +34,55 @@ export const createTaskAction = (slackClient: App) => {
   slackClient.view<ViewSubmitAction>(
     { callback_id: 'submit-form' },
     async ({ ack, payload, client }) => {
-      ack();
       const { values } = payload.state;
-      const slackAccountableUserEmail = await (
-        await client.users.profile.get({ user: values.accountable.accountable.selected_user })
-      ).profile.email;
+
+      const accountableId = values.accountable.accountable.selected_user;
+      const reviewerId = values.reviewer.reviewer.selected_user;
+
+      const slackAccountableUserEmail = (await client.users.profile.get({ user: accountableId }))
+        .profile.email;
       const accountable = await Notion.getUserByEmail(slackAccountableUserEmail);
 
-      const slackReviewerUserEmail = await (
-        await client.users.profile.get({ user: values.reviewer.reviewer.selected_user })
-      ).profile.email;
+      const slackReviewerUserEmail = (await client.users.profile.get({ user: reviewerId })).profile
+        .email;
       const reviewer = await Notion.getUserByEmail(slackReviewerUserEmail);
+
       const params = mapCreateTaskSubmitValuesToParams(values, accountable, reviewer);
+      const { error = null } = createTaskSchema.validate({ ...params });
 
-      await Notion.createTask(params);
-      // TODO: check if timeline start date is less than today and send error msg
-      // TODO: check if timeline end date is less than start date and send error msg
-      // Example error handling:
-      // ack({
-      //   response_action: 'errors',
-      //   errors: {
-      //     title: 'Please enter a title'
-      //   }
-      // });
-      // IMPORTANT: Please note that the errors object property names should match the action_id from payload.state.values
+      const isEndDateLaterThanStartDate =
+        new Date(params.timeline.end).getTime() - new Date(params.timeline.start).getTime() < 0;
 
-      // TODO: map the payload to notion create task params
+      if (isEndDateLaterThanStartDate && params.timeline.end !== null) {
+        await ack({
+          response_action: 'errors',
+          errors: {
+            end: 'End date should be less than start date!'
+          }
+        });
+      } else if (error) {
+        await ack({
+          response_action: 'errors',
+          errors: {
+            [error.details[0].context.key]: error.details[0].message
+          }
+        });
+      } else {
+        await ack();
+      }
+      const task = await Notion.createTask(params);
 
-      // TODO: call Notion.CreateTask with the mapped params and post ephemeral message with the task
+      const reviewerAssignedATaskMsg = `<@${reviewerId}> assigned you a task.`;
+      const msg = 'Task Created!';
+      await client.chat.postEphemeral({
+        channel: values.accountable.accountable.selected_user,
+        user: values.accountable.accountable.selected_user,
+        blocks: [
+          sectionBlock(accountableId === reviewerId ? msg : reviewerAssignedATaskMsg),
+          dividerBlock(),
+          sectionBlockUrl(task.name, task.url)
+        ]
+      });
     }
   );
 };
