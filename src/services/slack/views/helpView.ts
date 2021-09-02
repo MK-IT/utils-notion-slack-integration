40df3fe255d5d160ee3a4a75c6010cde5b174a7d
Martin Angelov <martoy123691@gmail.com>
2021-09-02 11:55:57 +0300

    fix: remove hardcoded slack comamnds from help view

--- src/services/slack/views/helpView.ts
+++ src/services/slack/views/helpView.ts
@@ -1,14 +1,26 @@
 import { KnownBlock } from '@slack/bolt';
 
+import {
+  SLACK_HELP_COMMAND_ARGUMENT,
+  SLACK_LIST_TASKS_COMMAND_ARGUMENT,
+  SLACK_MAIN_SLASH_COMMAND,
+  SLACK_NEW_TASK_COMMAND_ARGUMENT
+} from '@utils/constants';
 import { sectionBlock } from '../blocks';
 
 export const helpView = () => {
   const blocks: KnownBlock[] = [
     sectionBlock(`Hi there 👋, I'm Alfonso. I'm here to help you manage your tasks in Notion.`),
     sectionBlock('Currently there are :three: available commands:'),
-    sectionBlock('*:one: Create task.*. Type `/notion new` to create a task.'),
-    sectionBlock('*:two: List tasks.* Type `/notion tasks` to list incomplete tasks.'),
-    sectionBlock('*:three: Help.* Type `/notion help` to list available commands.')
+    sectionBlock(
+      `*:one: Create task.*. Type \`${SLACK_MAIN_SLASH_COMMAND} ${SLACK_NEW_TASK_COMMAND_ARGUMENT}\` to create a task.`
+    ),
+    sectionBlock(
+      `*:two: List tasks.* Type \`${SLACK_MAIN_SLASH_COMMAND} ${SLACK_LIST_TASKS_COMMAND_ARGUMENT}\` to list incomplete tasks.`
+    ),
+    sectionBlock(
+      `*:three: Help.* Type \`${SLACK_MAIN_SLASH_COMMAND} ${SLACK_HELP_COMMAND_ARGUMENT}\` to list available commands.`
+    )
   ];
   return blocks;
 };
